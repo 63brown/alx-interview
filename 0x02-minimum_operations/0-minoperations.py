815edacb407d1baa08d8396f20ac1b82546bc15d
63brown <mutheemumbi01@gmail.com>
2023-07-15 23:23:55 +0300

    min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 def minOperations(n):
-    if n == 1:
+    if n < 2:
         return 0
 
     operations = [0] * (n + 1)
     for i in range(2, n + 1):
         operations[i] = float('inf')
-        for j in range(1, i):
+        for j in range(1, i // 2 + 1):
             if i % j == 0:
                 operations[i] = min(operations[i], operations[j] + (i // j))
 
     return operations[n] if operations[n] != float('inf') else 0
-
